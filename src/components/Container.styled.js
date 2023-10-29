0065a9f41b4fb239eb4e75e890e5c32cd59106d2
Priyanshu0007 <63500076+Priyanshu0007@users.noreply.github.com>
2023-10-30 04:00:00 +0530

    readme updated

--- src/components/Container.styled.js
+++ src/components/Container.styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 export const Container=styled.div`
     width:1000px;
     max-width:100%;
-    padding:0 20px;
     margin:10% auto;
     display:flex;
     gap:9rem;
